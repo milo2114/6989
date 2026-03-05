7969dd55d5a6dc2580fb0aa0fba454499893bf8f
Eyder Hoyos G <hoyosgaleanoeider@gmail.com>
2026-03-05 16:37:36 -0500

    se agrego la funcion de trabajo al menu principal

--- principal.py
+++ principal.py
@@ -1,12 +1,13 @@
 import os 
 from saludo import saludo
 from despedida import despedida
+from trabajando import saludo_trabajando
 
 def clear():
     os.system("cls")
 
 def menu():
-    options= input ("1. saludo\n2. ask for childe\n3. farewell\n4. Exit\nEnter your choice: ")
+    options= input ("1. saludo\n2. ask for childe\n3. farewell\n4. working\n 5. Exist \n Enter your choice: ")
     if options == "1":
         saludo()    
     elif options == "2":
@@ -14,8 +15,10 @@ def menu():
     elif options == "3":
         print ("farewell my horse!")
     elif options == "4":
+        saludo_trabajando()
+    elif options == "5":
         despedida()
-        menu() 
+        menu()
     else:       
         print ("invalid option")
         clear()
