45f4abbb7a5c418652deb95e4b5ab99c4a3c06c0
Eyder Hoyos G <hoyosgaleanoeider@gmail.com>
2026-03-05 15:41:19 -0500

    se hizo todo

--- principal.py
+++ principal.py
@@ -1,21 +1,28 @@
 import os 
+from saludo import saludo
+from despedida import despedida
+
 def clear():
     os.system("cls")
 
 def menu():
     options= input ("1. saludo\n2. ask for childe\n3. farewell\n4. Exit\nEnter your choice: ")
     if options == "1":
-        print ("hellou my horse")
+        saludo()    
     elif options == "2":
         print ("how are you my horse?")
     elif options == "3":
         print ("farewell my horse!")
     elif options == "4":
-        print ("good bye my horse!")
-        clear()
+        despedida()
         menu() 
     else:       
         print ("invalid option")
         clear()
-menu()
-print ("good bye my horse!")
+        menu()
+
+if __name__ == "__main__":
+    menu()
+    print ("goodbye my laif")  
+
+
